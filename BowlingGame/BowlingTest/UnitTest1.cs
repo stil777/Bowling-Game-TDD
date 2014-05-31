c92ef8a79bf54f5f48a88cd768c563b7d8a49c44
STIL <stil3113@mail.ru>
2014-05-31 16:43:12 +0600

    Test two: denied

--- BowlingGame/BowlingTest/UnitTest1.cs
+++ BowlingGame/BowlingTest/UnitTest1.cs
@@ -8,12 +8,20 @@ namespace BowlingTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void testGutterGame()
         {
             Bowling game = new Bowling();
             for (int i = 0; i < 20; i++)
                 game.roll(0);
             Assert.AreEqual(0, game.score());
+        }
+        [TestMethod]
+        public void testAllOnes()
+        {
+            Bowling game = new Bowling();
+            for (int i = 0; i < 20; i++)
+                game.roll(1);
+            Assert.AreEqual(20, game.score());
         }
     }
 }
